11c303443406de0cd8b80800ca303c563a39cbe7
PLUCSCE <PLUCSCE@CSCE17185-M212>
2015-05-02 16:40:50 -0700

    new AI class

---
+++ src/ClientModel/AI.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ClientModel;
+
+/**
+ *
+ * @author PLUCSCE
+ */
+public class AI {
+    
+    int difficulty = 0;
+    
+    
+    
+    /**
+     * methods sets the difficulty rating
+     * 1 = easy, 2 = medium, 3 = impossible
+     * @param value 
+     */
+    public void setDifficulty(int value){
+        difficulty = value; 
+        
+    }
+    
+    /**
+     * method checks the board for a cases of 5 in a row
+     * @param theboard
+     * @param playerToken
+     * @return 
+     */
+     public boolean checkWin(int[][] theboard, int playerToken) {
+     if (checkRow(theboard, playerToken) || checkCol(theboard, playerToken) || checkDiag(theboard, playerToken)) {
+     return true;
+     }
+
+     return false;
+     }
+     
+     /**
+     * checks the Rows of the board to see if there are cases of 3+ in a row
+     *
+     * @param theboard the 2d array to be scanned
+     * @return true if the board contains 5 in a row
+     */
+    public int checkRow(int[][] theboard, int playerToken) {
+
+        int size = theboard.length;
+
+        for (int r = 0; r < size; r++) { // moves through the rows once col -4 have been checked
+            for (int c = 0; c < size - 4; c++) {
+                if (theboard[r][c] == playerToken && theboard[r][c + 1] == playerToken && theboard[r][c + 2] == playerToken ) {
+                    return 3;
+                }
+              
+            }
+        }
+
+        return 1;
+    }
+
+    /**
+     * checks the Cols of the board to see if there is 5 in a row
+     *
+     * @param theboard the 2d array to be scanned
+     * @return true if the board contains 5 in a row
+     */
+    public boolean checkCol(int[][] theboard, int playerToken) {
+
+        int size = theboard.length;
+
+        for (int c = 0; c < size; c++) { // moves through the rows once col -4 have been checked
+            for (int r = 0; r < size - r; r++) {
+                if (theboard[r][c] == playerToken && theboard[r + 1][c] == playerToken && theboard[r + 2][c] == playerToken
+                        && theboard[r + 3][c] == playerToken && theboard[r + 4][c] == playerToken) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * checks the diagonals of the board to see if there is 5 in a row
+     *
+     * @param theboard the 2d array to be scanned
+     * @return true if the board contains 5 in a row
+     */
+    public boolean checkDiag(int[][] theboard, int playerToken) {
+
+        int size = theboard.length;
+
+        for (int r = 0; r < size - 4; r++) { // moves through the rows once col -4 have been checked
+            for (int c = 0; c < size - 4; c++) {
+                if (theboard[r][c] == playerToken && theboard[r + 1][c + 1] == playerToken && theboard[r + 2][c + 2] == playerToken
+                        && theboard[r + 3][c + 3] == playerToken && theboard[r + 4][c + 4] == playerToken) {
+                    return true;
+                    // this checks the left and down diagnoal only when we are on the 4th col or further
+                } else if (c > 3 && theboard[r][c] == playerToken && theboard[r + 1][c - 1] == playerToken
+                        && theboard[r + 2][c - 2] == playerToken && theboard[r + 3][c - 3] == playerToken
+                        && theboard[r + 4][c - 4] == playerToken) {
+                    return true;
+                }
+
+            }
+        }
+
+        return false;
+    }
+    
+    
+    
+    
+}
